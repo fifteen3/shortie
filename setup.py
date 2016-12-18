fc2e24204e0653fb8c923b9a3438df6ba9e80fd1
Carlo Costantini <carlo@carlocostantini.ca>
2016-12-18 06:45:46 -0500

    create initial setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='shortie',
+      version='0.1',
+      description='Api for creating shortnened urls',
+      url='http://github.com/fifteen3/shortie',
+      author='Carlo Costantini',
+      author_email='carlo@carlocostantini.ca',
+      license='MIT',
+      packages=['shortie'],
+      zip_safe=False)
