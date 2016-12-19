edee4138f0d135c6df8587370eaf70deffc3cce6
Carlo Costantini <carlo@carlocostantini.ca>
2016-12-19 01:18:27 -0500

    add upgrade tests to use setup and teardown

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,20 @@
 import pytest
 import json
 import re
-from shortie import api
+from shortie import api,manage
 
-@pytest.fixture
-def client():
+@pytest.fixture(scope='function')
+def client(request):
+    #setup
     client = api.app.test_client()
-    return client
+    print "create db."
+    manage.create_db()
+    print "populate db."
+    manage.pop_db()
+    yield client
+    #teardown
+    print "delete db."
+    manage.delete_db()
 
 def test_root(client):
     response = client.get('/')
@@ -27,7 +35,7 @@ def test_shorten_url():
     shortie = api.shorten_url(long_hash)
     assert long_hash[:7] in  shortie
 
-def test_get_user_id():
+def test_get_user_id(client):
     email = 'jason@seaver.com'
     actual_user_id = api.get_user_id(email)
     expected = 1
@@ -35,15 +43,14 @@ def test_get_user_id():
 
 def test_store_urls(client):
     urls = { 'desktop' : 'https://test.desktop', 'mobile' : 'https://test.mobile', 'tablet' : 'https://test.tablet'}
-    payload = json.dumps({ 'urls' : urls })
+    actual = insert_case(client,urls)
     expected =  api.encode_url(urls['desktop'],'jason@seaver.com')[:7]
-    response = client.post('/api/v1/users/jason@seaver.com/urls', data=payload, content_type = 'application/json')
-    actual = json.loads(response.data)
     assert (expected in actual['data'][0]['shortie'])
 
 def test_redirect_desktop(client):
     expected = 'https://test.desktop'
-    actual = True
+    urls = { 'desktop' : 'https://test.desktop', 'mobile' : 'https://test.mobile', 'tablet' : 'https://test.tablet'}
+    insert_case(client,urls)
     ua_string = '''Mozilla/5.0 (MacOs) AppleWebKit/534.46 (KHTML, like Gecko)'''
     headers = { 'User-Agent' : ua_string }
     short_url = '/50617f9'
@@ -52,7 +59,8 @@ def test_redirect_desktop(client):
 
 def test_redirect_mobile(client):
     expected = 'https://test.mobile'
-    actual = True
+    urls = { 'desktop' : 'https://test.desktop', 'mobile' : 'https://test.mobile', 'tablet' : 'https://test.tablet'}
+    insert_case(client,urls)
     ua_string = '''Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3'''
     headers = { 'User-Agent' : ua_string }
     short_url = '/50617f9'
@@ -60,6 +68,8 @@ def test_redirect_mobile(client):
     assert (expected in actual)
 
 def test_redirect_tablet(client):
+    urls = { 'desktop' : 'https://test.desktop', 'mobile' : 'https://test.mobile', 'tablet' : 'https://test.tablet'}
+    insert_case(client,urls)
     ua_string = '''Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10'''
     expected = 'https://test.tablet'
     headers = { 'User-Agent' : ua_string }
@@ -68,9 +78,27 @@ def test_redirect_tablet(client):
     assert (expected in actual)
 
 def test_user_urls(client):
+    urls = { 'desktop' : 'https://test.desktop', 'mobile' : 'https://test.mobile', 'tablet' : 'https://test.tablet'}
+    response = insert_case(client,urls)
+    short_url = response['data'][0]['shortie']
+    request_redirect(client,short_url)
+    request_redirect(client,short_url)
+    request_redirect(client,short_url)
     rows = api.list_urls_by_user('jason@seaver.com')
     assert (len(rows) > 0)
     for row in rows:
         assert (row['url'])
-        assert (row['url_type'])
-        assert (row['visits'])
+        assert (row['url_type'] and re.match("mobile|tablet|desktop",row['url_type']))
+        assert (row['visits'] and row['visits'] == 3)
+        assert (row['since'])
+
+def insert_case(tclient,urls):
+    payload = json.dumps({ 'urls' : urls })
+    response = tclient.post('/api/v1/users/jason@seaver.com/urls', data=payload, content_type = 'application/json')
+    actual = json.loads(response.data)
+    return actual
+
+def request_redirect(tclient,short_url):
+    ua_string = '''Mozilla/5.0(iPad; U; CPU iPhone OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B314 Safari/531.21.10'''
+    headers = { 'User-Agent' : ua_string }
+    actual = tclient.get(short_url,headers=headers).data
