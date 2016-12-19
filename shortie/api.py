4153b38b1ecad00d4041e000465d5028585772f8
Carlo Costantini <carlo@carlocostantini.ca>
2016-12-18 19:52:15 -0500

    add shorten_url and encode_url methods

--- shortie/api.py
+++ shortie/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort, redirect
 from werkzeug.contrib.fixers import ProxyFix
+import hashlib
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
@@ -8,6 +9,19 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 def root_route():
     return "Welcome to Shortie."
 
+
+
+def shorten_url(long_hash):
+    host = 'http://localhost:5000'
+    shortened_url = "{}{}".format(host,  long_hash[:7])
+    return shortened_url
+
+def encode_url(url,user):
+    url_bytes = url.encode('utf-8')
+    user_bytes = user.encode('utf-8')
+    m = hashlib.sha1(url_bytes + user_bytes)
+    long_hash = m.hexdigest()
+    return long_hash
 
 if __name__ == '__main__':
     app.run()
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,5 @@
 import pytest
+import re
 from shortie import api
 
 @pytest.fixture
@@ -9,3 +10,19 @@ def client():
 def test_root(client):
     response = client.get('/')
     assert u'Welcome to Shortie.' in response.data
+
+def test_encode():
+    url = "http://www.foo.com"
+    user = "user@users.com"
+    long_hash = api.encode_url(url,user)
+    pattern = '[A-Za-z0-9]+'
+    assert re.match(pattern, long_hash)
+
+def test_shorten_url():
+
+    url = "http://www.foo.com"
+    user = "user@users.com"
+    long_hash = api.encode_url(url,user)
+    shortie = api.shorten_url(long_hash)
+    assert long_hash[:7] in  shortie
+
