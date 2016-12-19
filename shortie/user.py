da119d11c11e76a3e42f81f0440ba0c2b99c75e7
Carlo Costantini <carlo@carlocostantini.ca>
2016-12-19 17:40:45 -0500

    make the db module a dependency not a global

--- shortie/user.py
+++ shortie/user.py
@@ -1,12 +1,13 @@
-import db
 import sqlite3
 
 class User:
-    def __init__(self,email):
-        self.email = email
+    """User class for getting user info and list of urls"""
+    def __init__(self,**kwargs):
+        self.email = kwargs.get('email')
+        self.db = kwargs.get('db')
 
     def get_user_id(self):
-        c = db.get_db().cursor()
+        c = self.db.get_db().cursor()
         if not c:
             return None
         try:
@@ -18,7 +19,7 @@ class User:
 
     def list_urls(self):
         user_id = self.get_user_id()
-        conn = db.get_db()
+        conn = self.db.get_db()
         c = conn.cursor()
         list_query = '''SELECT urls.url as url,v.url_type as url_type,COUNT(v.short_hash) as visits, (julianday('now') - julianday(hashes.created)) as since FROM users AS u JOIN url_hashes AS hashes ON (u.rowid = hashes.user_id) JOIN urls USING (short_hash) JOIN visits AS v ON (urls.short_hash = v.short_hash AND urls.url_type = v.url_type) WHERE u.rowid = ? GROUP BY v.url_type'''
         c.execute(list_query, [user_id])
